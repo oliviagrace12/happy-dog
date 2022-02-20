422d31f70855bbba900300f8128c51c6b0709922
Olivia Rojas <olivia.chisman@gmail.com>
2022-02-20 15:36:11 -0600

    Initial Commit

---
+++ Happy Dog/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Happy Dog
+//
+//  Created by Olivia Chisman on 2/20/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
