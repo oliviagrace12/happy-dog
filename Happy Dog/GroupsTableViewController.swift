3d185da0823e4170632b973ec638606ad6f90573
Olivia Rojas <olivia.chisman@gmail.com>
2022-03-06 22:21:37 -0600

    groups started

---
+++ Happy Dog/GroupsTableViewController.swift
@@ -0,0 +1,105 @@
+//
+//  GroupsTableViewController.swift
+//  Happy Dog
+//
+//  Created by Olivia Chisman on 3/6/22.
+//
+
+import UIKit
+
+class GroupsTableViewController: UITableViewController {
+    
+    let dataParser: DataParser = DataParser()
+    var groupDictionary: Dictionary<String, Breed> = Dictionary()
+    var groups: Array<String> = Array()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        if (breeds.isEmpty) {
+            if let data = dataParser.readLocalFile(forName: "breeds") {
+                breeds = dataParser.parse(jsonData: data)
+            }
+        }
+        
+        for breed in breeds {
+            let breedGroup: String = breed.breed_group != nil ? breed.breed_group! : "Other"
+            groupDictionary[breedGroup] = breed
+            groups.append(breedGroup)
+        }
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return groups.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "basicGroup", for: indexPath)
+
+        cell.textLabel?.text = groups[indexPath.row]
+
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
